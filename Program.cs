db246ceb4e9b21a20bf5486665e4c81f6a583b0a
abbas <abbas@ABBAS>
2022-08-24 20:07:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =Int32.Parse( Console.ReadLine());
+            
+            int b = Int32.Parse(Console.ReadLine());
+            
+            int s = a + b;
+            Console.WriteLine(s);
+        }
+    }
+}
